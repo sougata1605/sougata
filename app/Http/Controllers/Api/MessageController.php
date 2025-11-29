4cc6fdb903161cde0a3dd90071c8aae6019e1ada
Sougata Chatterjee <chatterjee2014@gmail.com>
2025-11-29 12:06:47 +0530

    message controller

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -1,11 +1,35 @@
 <?php
 
+
 namespace App\Http\Controllers\Api;
+
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Message;
+use App\Jobs\ProcessMessage;
+
 
 class MessageController extends Controller
 {
-    //
+public function store(Request $request)
+{
+$request->validate([
+'sender_id' => 'required|integer',
+'message' => 'required|string',
+]);
+
+
+$message = Message::create([
+'sender_id' => $request->sender_id,
+'message' => $request->message,
+]);
+
+
+
+ProcessMessage::dispatch($message->id);
+
+
+return response()->json(['message_id' => $message->id], 201);
+}
 }
